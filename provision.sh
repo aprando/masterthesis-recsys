f6603afacbc7900204778c0f9396b4767862e4ae
Alan Prando <alan@scanboo.com.br>
2015-07-04 18:15:57 -0300

    add locale on bashrc

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
  
-apt-get update
-
+echo 'export LC_ALL="en_US.UTF-8"' >> /home/vagrant/.bashrc
  
 echo '----------------------------------------------'
 echo ' INSTALLING JAVA, GIT and MAVEN'
 echo '----------------------------------------------'
+apt-get update
 apt-get -y --force-yes install \
 openjdk-7-jdk \
 git \
@@ -23,3 +23,4 @@ echo ' INSTALLING TOMCAT'
 echo '----------------------------------------------'
 sudo apt-get update
 sudo apt-get install -y tomcat7
+
